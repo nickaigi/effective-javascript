dc259e1ddef855a1ec90e8be304fdee52c889d0b
Nickson Kaigi <nickaigi@gmail.com>
2019-04-29 19:10:55 +0300

    taking a break, logical or

--- chapter_1/script.js
+++ chapter_1/script.js
@@ -5,8 +5,28 @@
 const PI = 3.141592653589793;
 
 // All numbers in js are double precision floating point numbers AKA doubles
-typeof 17;    //number
+typeof 17;    // number
 
-typeof 98.6;  //number
+typeof 98.6;  // number
 
-typeof -2.1;  //number
+typeof -2.1;  // number
+
+// most arithmetic operators work with integers, real numbers, or a combination
+// of the two
+0.1 * 1.9; // 0.19
+
+-99 + 100; // 1
+
+21 - 12.3; // 8.7
+
+2.5 / 5;   // 0.5
+
+21 % 8     // 5
+
+// bitwise arithmetic operators are special
+8 | 1; // 9
+
+(8).toString(2) // 1000
+// toString specifies the radix, 2, indicating binary representation
+
+// Js represents the
