bc2160d67ca69852e799f16e9e2a6d6f93fe56d0
Nickson Kaigi <nickaigi@gmail.com>
2019-04-29 18:07:24 +0300

    all numbers in js are doubles

---
+++ chapter_1/script.js
@@ -0,0 +1,12 @@
+// Do not concatenate 'strict' files and 'unstrict' files
+"use strict";
+
+// const variables are immutable
+const PI = 3.141592653589793;
+
+// All numbers in js are double precision floating point numbers AKA doubles
+typeof 17;    //number
+
+typeof 98.6;  //number
+
+typeof -2.1;  //number
