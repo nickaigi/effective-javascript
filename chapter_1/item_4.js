b2d8dafd216cde4c11016857b56be7b12318a028
Nickson Kaigi <nickaigi@gmail.com>
2019-05-01 09:03:45 +0300

    initial import of item_4

---
+++ chapter_1/item_4.js
@@ -0,0 +1,20 @@
+// prefer primitives to Object Wrappers
+// Js has 5 types of primitives
+// booleans, numbers, strings, null, undefined
+
+// you can create a 'String' object that wraps a string value
+var s = new String("hello");
+
+s + " world";  // "hello world"
+
+s[4]; // "o"
+
+// unlike primitive strings, a String object is a true object
+typeof s;       // object
+typeof "hello"  // string
+
+// means you can not compare the contents of two distinct 'String' objects
+// using built-in operators
+var s1 = new String("hello");
+var s2 = new String("hello");
+s1 === s2; //fasle
