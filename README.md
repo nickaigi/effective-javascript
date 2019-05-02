74a61a86d71256ea317c23ab4368e4c1555378b6
Nickson Kaigi <nickaigi@gmail.com>
2019-05-02 18:03:12 +0300

    moved index.html, added chapter2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # effective_javascript
+
 Tips from "Effective Javascript" by David Herman
+
+## Notes:
+
+- item 6: Always use Semicolons
---
+++ chapter_2/item_8.js
@@ -0,0 +1,37 @@
+// minimize Use of the Global object
+//
+// Defining global variables pollutes the common namespace shared by everyone,
+// introducing the possibility of accidental name collisions.
+
+function averageScore(players) {
+    var i, n, sum;  // locals
+    sum = 0;
+    for (i = 0, n = players.length; i < n; i++) {
+        sum += score(players[i]);
+    }
+    return sum / n;
+}
+
+function score(player) {
+    var i, n , sum;
+    sum = 0;
+    for (i = 0, n = player.levels.length; i < n; i++) {
+        sum += player.levels[i].score;
+    }
+    return sum;
+}
+
+// JavaScript's global namespace is exposed as a global object - 'this'
+
+this.foo;  //undefined
+foo = "global foo";
+this.foo;  // "global foo"
+
+// there are two ways to define globals:
+// 1. declare it with 'var' in the global space
+// 2. add it to the global object
+var foo = "global foo";
+console.log(foo);     // "global foo"
+this.foo = "changed";
+console.log(foo);     // "changed"
+
