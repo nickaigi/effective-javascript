c4700a3c6fba54d718b3279ff0ea9fcb1bb3dcf6
Nickson Kaigi <nickaigi@gmail.com>
2019-05-07 13:20:43 +0300

    arguments is an alias of the named parameters

--- chapter_3/item_23.js
+++ chapter_3/item_23.js
@@ -9,3 +9,48 @@ var names = ["Nick", "Son", "Mr", "Kaigi"];
 names.shift();
 
 names;  //  ["Son", "Mr", "Kaigi"]
+
+function callMethod(obj, method) {
+    var shift = [].shift;
+    shift.call(arguments);
+    shift.call(arguments);
+    return obj[method].apply(obj, arguments);
+}
+
+var obj = {
+    add: function(x, y) { return x + y; }
+};
+
+callMethod(obj, "add", 17, 25);
+// Uncaught TypeError: Cannot read property 'apply' of undefined
+//
+// fails because 'arguments' object is not a copy of the function's arguments
+// all named arguments are 'aliases' to their corresponding indices in the
+// arguments object.
+// So 
+// 'obj' alias for arguments[0]
+// 'method' alias for arguments[1]
+// even after we remove elements from the 'arguments' object via 'shift'
+
+// L15, L16 we called shift twice, to shift the arguments array until we
+// arrive at [17, 25]
+// just feels like a hack!
+
+// function parameters in 'strict' mode do not alias their 'arguments' object
+function strict(x) {
+    "use strict";
+    arguments[0] = "modified";
+    console.log(arguments);
+    return x === arguments[0];
+}
+
+function nostrict(x) {
+    arguments[0] = "modified";
+    console.log(arguments);
+    return x === arguments[0];
+}
+
+strict("unmodified");    // false
+// why does this happen?
+// x = "unmodified" and arguments[0] = "unmodified"
+nostrict("unmodified");  // true
