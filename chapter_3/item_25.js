5a51042fd8e1432a98dd8854c08b0e57055a2ad7
Nickson Kaigi <nickaigi@gmail.com>
2019-05-07 17:02:42 +0300

    initial import

---
+++ chapter_3/item_25.js
@@ -0,0 +1,11 @@
+// use 'bind' to extract methods with a fixed receiver
+
+var buffer = {
+    entries: [],
+    add: function(s) {
+        this.entries.push(s);
+    },
+    concat: function() {
+        return this.entries.join("");
+    }
+};
