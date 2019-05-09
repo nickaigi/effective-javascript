c364d138ff3e8f7e17a5b367685118750964e2ef
Nickson Kaigi <nickaigi@gmail.com>
2019-05-09 21:57:52 +0300

    done

--- chapter_3/item_28.js
+++ chapter_3/item_28.js
@@ -14,6 +14,24 @@ console.log(myF);
 
 var hiddenF = (function(x) {
     return x + 1;
-}).bind(16).toString();  // "function () { [native code] }"
+}).bind(16).toString();  // "function () {\n    [native code]\n}"
 
 console.log(hiddenF);
+
+// in many host envs, the 'bind' function is implemented in another programming
+// language (typically C++), it produces a compiled function that has no
+// Js code to reveal.
+
+
+// toString does not provide a representation of closures that preserve the
+// values associated with their inner variable references
+
+var closureF = (function(x) {
+    return function(y) {
+        return x + y;
+    }
+})(42).toString();
+
+console.log(closureF);  //function(y) {\n    return x + y;\n}
+// notice how the resultant string still contains a variable reference to x,
+// even though the function is actually a closure that binds x to 42
