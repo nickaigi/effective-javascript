9ae56cf16565ec584cedd9ea72bc35d86bedb5d3
Nickson Kaigi <nickaigi@gmail.com>
2019-05-09 16:17:16 +0300

    buffer.contat

--- chapter_3/item_25.js
+++ chapter_3/item_25.js
@@ -13,5 +13,21 @@ var buffer = {
 var source = ["867", "-", "5309"];
 // source.forEach(buffer.add); // error
 // fails because 'forEach' uses the global object as the receiver.
-// fix
+// fix, use L17
 source.forEach(buffer.add, buffer);
+buffer.concat();  // "867-5309"
+
+
+// not all higher-order functions can accept the extra reciever argument
+// Soln. create a local function that makes sure to call buffer.add with the
+// appropriate method call syntax
+buffer.entries = [];
+var source = ["867", "-", "5309"];
+
+source.forEach(function(s) {
+    buffer.add(s);
+});
+buffer.concat();  // "867-5309"
+
+// L27 creates a wrapper function tht explicitly calls 'add' as a method of
+// buffer. Notice the wrapper function does not refer to 'this'
