d2adbf056cc776b6a4a212acacba202e865240e2
Nickson Kaigi <nickaigi@gmail.com>
2019-05-09 22:17:19 +0300

    caller

--- chapter_3/item_29.js
+++ chapter_3/item_29.js
@@ -12,9 +12,25 @@
 // functions to refer to themeselves recursively
 var factorial = (function(n) {
     return (n <= 1) ? 1 : (n * arguments.callee(n - 1));
-};
+});
 
 // better just have a function refer to itself by its name
 function factorial(n) {
     return (n <= 1) ? 1 : (n * factorial(n - 1));
 }
+
+// arguments.caller refers to the function that made the call with the given
+// arguments object
+// WARNING: Has been removed from most environments for security
+// 
+// Caveat Emptor: some envs have a 'caller' property which refers to the
+// functions most recent caller
+function revealCaller(){
+    return revealCaller.caller;
+}
+
+function start() {
+    return revealCaller()
+}
+
+start() === start; // true
