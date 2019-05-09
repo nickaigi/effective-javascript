3b4ccf2bcf2431d6e501c907493e47205249d6d9
Nickson Kaigi <nickaigi@gmail.com>
2019-05-09 16:55:29 +0300

    explained how .bind() works

--- chapter_3/item_25.js
+++ chapter_3/item_25.js
@@ -22,7 +22,6 @@ buffer.concat();  // "867-5309"
 // Soln. create a local function that makes sure to call buffer.add with the
 // appropriate method call syntax
 buffer.entries = [];
-var source = ["867", "-", "5309"];
 
 source.forEach(function(s) {
     buffer.add(s);
@@ -31,3 +30,12 @@ buffer.concat();  // "867-5309"
 
 // L27 creates a wrapper function tht explicitly calls 'add' as a method of
 // buffer. Notice the wrapper function does not refer to 'this'
+// ES5 has a library function that achieves the above functionality
+buffer.entries = [];
+source.forEach(buffer.add.bind(buffer));
+buffer.concat();  // "867-5309"
+
+// bear in mind: buffer.add.bind(buffer) creates a 'new' funciton rather than
+// modifying the old one, but with its receiver bound to buffer, while the old
+// one remains unchanged
+buffer.add === buffer.add.bind(buffer);  //false
