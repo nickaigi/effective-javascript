5ca5665b6d7de69e897dbfb17c0ff7eb3b820852
Nickson Kaigi <nickaigi@gmail.com>
2019-05-10 19:16:00 +0300

    can i do the Caesar cipher

--- chapter_4/item_30.js
+++ chapter_4/item_30.js
@@ -17,6 +17,31 @@
 //        prototype object
 
 
+// make use of Ceasar cipher to hash our password https://en.wikipedia.org/wiki/Caesar_cipher
+
+var charCodeStart = 65; // charcode for "A"
+var charCodeStart = 65; // charcode for "A"
+
+var alphabet = {
+    plain: function() {
+        var result = [];
+        for(var i = 0; i < 26; i++) {
+            result.push(String.fromCharCode(charCodeStart + i));
+        }
+        return result;
+
+    },
+    cipher: function() {
+        var result = [];
+        for(var i = 0; i < 26; i++) {
+            result.push(String.fromCharCode(charCodeStart + i));
+        }
+        return result;
+    }
+};
+
+function stupidHash(password) {
+}
 function User(name, passwordHash) {
     this.name = name;
     this.passwordHash = passwordHash;
@@ -34,3 +59,8 @@ User.prototype.checkPassword = function(password) {
 
 var u = new User("nkaigi",
     "0ef33ae791068ec64b502d6cb0191387");
+
+Object.getPrototypeOf(u) === User.prototype; // true
+
+// if your env doesn't support ES5
+u.__proto__ === User.prototype
