fe136496bc993df0c8cfaec10aa2b7984fd20455
Nickson Kaigi <nickaigi@gmail.com>
2019-05-14 17:30:00 +0300

    added the diagram

--- chapter_4/item_34.js
+++ chapter_4/item_34.js
@@ -11,3 +11,50 @@ function User(name, passwordHash) {
         return hash(password) === this.passwordHash;
     };
 }
+
+// if we created 3 user instances
+var u1 = User("u1", "ZXCVB");
+var u2 = User("u2", "UIOP");
+var u3 = User("u3", "QWERTY");
+
+// Instead of sharing the  'toString' and 'checkPassword' methods via the
+// 'prototype', each instance contains a copy of both methods for a total of
+// six function objects
+//
+// Storing methods on instance objects
+// -----------------------------------
+//
+//
+//                    User.prototype
+//                  ----------------------
+//                  |                    |
+//        --------| ---------------------- |-----------
+//        |                    |                      |
+//        |                    |                      |
+//     prototype            prototype              prototype
+//        |                    |                      |
+// ------------------     ------------------      ----------------
+// |  .toString     |     |  .toString     |      |  .toString     |
+// |  .checkPassword|     |  .checkPassword|      |  .checkPassword|
+// |  .name         |     |  .name         |      |  .name         |
+// |  .passwordHash |     |  .passwordHash |      |  .passwordHash |
+// ------------------     ------------------      ------------------
+//
+//
+// Storing methods on a prototype object
+// -------------------------------------
+//
+//
+//                      User.prototype
+//                  ----------------------
+//                  |  .toString         |
+//                  |  .checkPassword    |
+//        --------| ---------------------- |-----------
+//        |                     |                      |
+//        |                     |                      |
+//     prototype             prototype              prototype
+//        |                     |                      |
+// ------------------     ------------------      ----------------
+// |  .name         |     |  .name         |      |  .name         |
+// |  .passwordHash |     |  .passwordHash |      |  .passwordHash |
+// ------------------     ------------------      ------------------
