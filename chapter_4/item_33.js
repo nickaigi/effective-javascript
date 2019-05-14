bb80240abae64aa0e0009546f3774928c0401a1d
Nickson Kaigi <nickaigi@gmail.com>
2019-05-14 11:50:57 +0300

    robust constructor

--- chapter_4/item_33.js
+++ chapter_4/item_33.js
@@ -12,3 +12,22 @@ var u = User("nickaigi", "XYZ");
 u;                  // undefined
 this.name;          // "nickaigi"
 this.passwordHash;  // "XYZ"
+
+// if the 'User' function is defined as ES5 strict code, then the receiver
+// defaults to 'undefined'
+function User(name, passwordHash) {
+    "use strict";
+    this.name = name;
+    this.passwordHash = passwordHash;
+}
+
+var u = User("nickaigi", "XYZ"); // TypeError: can not set property name of undefined
+
+// check that the receiver value is an instance of 'User'
+function User(name, passwordHash) {
+    if (!(this instanceof User)) {
+        return new User(name, passwordHash);
+    }
+    this.name = name;
+    this.passwordHash = passwordHash;
+}
