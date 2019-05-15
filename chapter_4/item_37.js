ccbd0c6be06919a522cac0724c03a9434724f241
Nickson Kaigi <nickaigi@gmail.com>
2019-05-15 18:53:24 +0300

    initial 37

---
+++ chapter_4/item_37.js
@@ -0,0 +1,19 @@
+// recognize them implicit binding of this
+
+function CSVReader(separators) {
+    this.separators = separators || [","];
+    this.regexp =
+        new RegExp(this.separators.map(function(sep) {
+            return "\\" + sep[0];
+        }).join("|"));
+}
+
+CSVReader.prototype.read = function(str) {
+    var lines = str.trim().split(/\n/);
+    return lines.map(function(line) {
+        return line.split(this.regexp);  // wrong this!
+    });
+};
+
+var reader = new CSVReader();
+reader.read("a,b,c\nd,e,f\n");  // [["a,b,c"], ["d,e,f"]]
