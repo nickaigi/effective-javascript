40653eb71fe0a84b5eaa3dd66ea6824407d6860c
Nickson Kaigi <nickaigi@gmail.com>
2019-05-15 15:57:05 +0300

    done

--- chapter_4/item_36.js
+++ chapter_4/item_36.js
@@ -21,11 +21,40 @@ var right = new Tree(6);
 right.addChild(5);
 right.addChild(7);
 
-var top_ = new Tree(4);
-top_.addChild(left);
-top_.addChild(right);
+// top refers to window.top
+// top === window.top; // true
 
-top_.children;  // [1, 3, 5, 7, left, right]
+var myTop = new Tree(4);
+myTop.addChild(left);
+myTop.addChild(right);
+
+myTop.children;  // [1, 3, 5, 7, left, right]
 
 // each call to 'addChild' appends a value to 'Tree.prototype.children'
 // which contains the nodes in the order of 'any' calls to 'addChild' anywhere
+
+// correct version
+function Tree(x) {
+    this.value = x;
+    this.children = [];  // instance state
+}
+
+Tree.prototype = {
+    addChild: function(x) {
+        this.children.push(x);
+    }
+};
+
+var left = new Tree(2);
+left.addChild(1);
+left.addChild(3);
+
+var right = new Tree(6);
+right.addChild(5);
+right.addChild(7);
+
+var myTop = new Tree(4);
+myTop.addChild(left);
+myTop.addChild(right);
+
+myTop.children;  // 
