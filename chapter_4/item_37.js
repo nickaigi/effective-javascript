c5708d1d298b304f7e665ef9f9b7bc2474302d03
Nickson Kaigi <nickaigi@gmail.com>
2019-05-15 21:17:47 +0300

    added forward binding this

--- chapter_4/item_37.js
+++ chapter_4/item_37.js
@@ -17,3 +17,26 @@ CSVReader.prototype.read = function(str) {
 
 var reader = new CSVReader();
 reader.read("a,b,c\nd,e,f\n");  // [["a,b,c"], ["d,e,f"]]
+// results in [Array(1), Array(1)]
+
+// the callback passed to 'lines.map' refers to 'this' expecting to extract the
+// 'regexp' property of the 'CSVReader' object
+//
+// But 'map' binds its callback's receiver to the 'lines' array, which doesn't
+// have such a property
+// therefore: 'this.regexp' is 'undefined'
+//
+// soln.: similar to 'forEach' example in item 25, the 'map' method of arrays
+// takes an optional second argument to use as 'this-binding' for the callback
+// Fix: forward the outer binding of 'this'
+
+CSVReader.prototype.read = function(str) {
+    var lines = str.trim().split(/\n/);
+    return lines.map(function(line) {
+        return line.split(this.regexp);  // wrong this!
+    }, this);
+};
+
+var reader = new CSVReader();
+reader.read("a,b,c\nd,e,f\n");  // [["a", "b ", "c"], ["d", "e", "f"]]
+// results in [Array(3), Array(3)]
