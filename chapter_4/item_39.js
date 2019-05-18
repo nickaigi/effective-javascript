5ae199439ad2759fe959bfe93e6e729e23d0be92
Nickson Kaigi <nickaigi@gmail.com>
2019-05-18 10:23:36 +0300

    use distinct property names

--- chapter_4/item_39.js
+++ chapter_4/item_39.js
@@ -12,3 +12,47 @@ function Actor(scene, x, y) {
 
 Actor.nextID = 0;
 
+
+function Alien(scene, x, y, direction, speed, strength) {
+    Actor.call(this, scene, x, y);
+    this.direction = direction;
+    this.speed = speed;
+    this.strength = strength;
+    this.damage = 0;
+    this.id = ++Alien.nextID;  // conflicts with actor id
+}
+
+Alien.nextID = 0;
+
+// both classes attempt to write to an instance property called 'id'
+// the property is stored on instance objects and named with a string
+//
+// if two classes in an inheritance hierarchy refer to the same property name
+// they will refer to the same property
+//
+// subclasses must always be aware of all properties used by their superclasses
+// Soln. use distinct property names
+
+
+function Actor(scene, x, y) {
+    this.scene = scene;
+    this.x = x;
+    this.y = y;
+    this.actorID = ++Actor.nextID; // distinct
+    scene.register(this);
+}
+
+
+Alien.nextID = 0;
+
+
+function Alien(scene, x, y, direction, speed, strength) {
+    Actor.call(this, scene, x, y);
+    this.direction = direction;
+    this.speed = speed;
+    this.strength = strength;
+    this.damage = 0;
+    this.alienID = ++Alien.nextID;  // distinct
+}
+
+Alien.nextID = 0;
