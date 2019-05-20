136ac142cb5ce8182e68dffa187ab2cd31442bfd
Nickson Kaigi <nickaigi@gmail.com>
2019-05-20 13:22:56 +0300

    done

--- chapter_4/item_40.js
+++ chapter_4/item_40.js
@@ -21,3 +21,42 @@ dir.length;   // 0
 
 // fails because 'length' property operates specifically on objects that are
 // marked internally as 'true' arrays
+//
+// The ECMAScript standard specifies this as an invinsible 'internal property'
+// called [[Class]] - whose value is a simple tag
+//
+// Array constructor or the [] - [[Class]] is "Array"
+//
+// instances of 'Dir' have [[Class]] "Object"
+//
+// Object.prototype.toString queries the internal [[Class]] property of its
+// receiver
+
+var dir = new Dir("/", []);
+Object.prototype.toString.call(dir);  // "[Object Object]"
+Object.prototype.toString.call([]);   // "[Object Array]"
+
+/// better
+function Dir(path, entries) {
+    this.path = path;
+    this.entries = entries;  // array property
+}
+
+// Array methods can be redefined on the property by delagating the
+// corresponding methods of the entries property
+
+Dir.prototype.forEach = function(f, thisArg) {
+    if (typeof thisArg === "undefined") {
+        thisArg = this;
+    }
+    this.entries.forEach(f, thisArg);
+};
+
+// therefore: avoid inheriting from
+// - Array
+// - Boolean
+// - Date
+// - Function
+// - Number
+// - RegExp
+// - String
