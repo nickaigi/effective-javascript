68c9f27cf910b134ee7b1eb387f11cd739135737
Nickson Kaigi <nickaigi@gmail.com>
2019-05-20 21:57:18 +0300

    monkey-patching

--- chapter_4/item_42.js
+++ chapter_4/item_42.js
@@ -8,3 +8,23 @@
 Array.prototype.split = function(i) {  // alternative 1
     return [this.slice(0, i), this.slice(i)];
 };
+
+// The slice() method returns a shallow copy of a portion of an array into a
+// new array object selected from 'begin' to 'end' (end not included).
+// The original array will not be modified.
+
+var myArr = [1,2,3,4,5,6,7,8]
+myArr.split(4);  // [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+
+// problems scenario:
+// multiple libraries monkey-patch the smae prototypes in incompatible ways.
+// Another library might monkey-patch Array.prototype with a method of the same
+// name
+Array.prototype.split = function() { // alternative 2
+    var i = Math.floor(this.length / 2);
+    return [this.slice(0, i), this.slice(i)];
+};
+
+// RECOMENDED: any library that modifies shared protoypes such as
+// 'Array.prototype' should clearly document that it does so.
