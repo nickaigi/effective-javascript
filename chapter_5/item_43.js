88421ac87c4c92e30e33c424d7a8d88b5447ae93
Nickson Kaigi <nickaigi@gmail.com>
2019-05-21 21:03:14 +0300

    prototype pollution

--- chapter_5/item_43.js
+++ chapter_5/item_43.js
@@ -39,3 +39,60 @@ dict.count(); // 5
 
 // problem is that we are using the same object to store both the fixed
 // properties (count, toString) and the variable entries (nick, son, kaigi)
+//
+// A similar mistake is to use the 'Array' type to represent dictionaries
+// example below
+// this code is vulnerable to 'prototype pollution'
+// where properties on a prototype object can cause unexpected properties to
+// appear when enumerating dictionary entries
+
+var dict = new Array();
+
+dict.nick = 16;
+dict.son = 32;
+dict.kaigi = 64;
+
+dict.nick; // 16
+
+// Eg another library in the application may decide to add some convenience
+// methods to 'Array.prototype'
+
+Array.prototype.first = function() {
+    return this[0];
+};
+
+Array.prototype.last = function() {
+    return this[this.length - 1];
+};
+
+// now try to enumerate elements of the array
+
+var names = [];
+
+for (var name in dict) {
+    names.push(name);
+}
+
+names;  // ["nick", "son", "kaigi", "first", "last"]
+
+// Cardinal Rule: only use direct instances of 'Object' as dictionaries
+
+var dict = {};
+
+dict.nick = 16;
+dict.son = 32;
+dict.kaigi = 64;
+
+var names = [];
+
+for (var name in dict) {
+    names.push(name);
+}
+
+names;  // ["nick", "son", "kaigi"]
+
+// While this implentatin is not safe from pollution,
+// but by using 'Object' we localize the risk to 'Object.prototype' alone
+//
+// see item_47: NEVER add properties to 'Object.prototype' that could pollute
+// a 'for...in' loop
