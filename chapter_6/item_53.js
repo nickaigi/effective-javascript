6ab402dfb570a2d1c32fd282bd96193ca0e02462
Nickson Kaigi <nickaigi@gmail.com>
2019-05-28 13:23:06 +0300

    item_53

---
+++ chapter_6/item_53.js
@@ -0,0 +1,8 @@
+// Maintain consitent conventions
+
+
+/* follow accepted conventions e.g. 'width' always comes before 'height'
+ * stick with what's familiar
+ */
+
+var widget = new Widget(320, 240);  // width: 320, height 240
