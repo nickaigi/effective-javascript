20441a0a5612497e18667b86c502547495d81452
Nickson Kaigi <nickaigi@gmail.com>
2019-06-01 10:50:05 +0300

    complete with an extend class

--- chapter_6/item_55.js
+++ chapter_6/item_55.js
@@ -41,6 +41,79 @@ function Alert(parent, message, opts) {
     this.bgColor = opts.bgColor || "white";
     this.textColor = opts.textColor || "black";
     this.icon = opts.icon || "info";
-    this.modal = !!opts.modal;
+    this.modal = !!opts.modal; // force opts.modal to a boolean with double negation pattern !!
     this.message = message;
+}
+
+/* double NOT(!!)
+ * https://stackoverflow.com/q/784929/1010338
+ *     Bang, bang you're a boolean :-)
+ *
+ * https://stackoverflow.com/a/784946/1010338
+ *     Coerces to boolean. if it was falsey (0, null, undefined) it will 'false' otherwise 'true'
+ */
+
+// lets use an object 'extension' or a 'merging' function
+
+function Alert(parent, message, opts) {
+    opts = extend({
+        width: 320,
+        height: 240
+    });
+    opts = extend({
+        x: (parent.width / 2) - (opts.width / 2),
+        y: (parent.height / 2) - (opts.height / 2),
+        title: "Alert",
+        titleColor: "gray",
+        bgColor: "white",
+        textColor: "black",
+        icon: "info",
+        modal: false
+    }, opts);
+    this.width  = opts.width;
+    this.height = opts.height;
+    this.x = opts.x;
+    this.y = opts.y;
+    this.title = opts.title;
+    this.titleColor = opts.titleColor;
+    this.bgColor = opts.bgColor;
+    this.textColor = opts.textColor;
+    this.icon = opts.icon;
+    this.modal = opts.modal;
+}
+
+// notice: use two calls to 'extend' since the default values for 'x' and 'y' depend
+// on first computing the values of 'width' and 'height'
+
+function Alert(parent, message, opts) {
+    opts = extend({
+        width: 320,
+        height: 240
+    });
+    opts = extend({
+        x: (parent.width / 2) - (opts.width / 2),
+        y: (parent.height / 2) - (opts.height / 2),
+        title: "Alert",
+        titleColor: "gray",
+        bgColor: "white",
+        textColor: "black",
+        icon: "info",
+        modal: false
+    }, opts);
+    extend(this, opts);
+}
+
+// typical implementation of 'extend' works by enumerating the properties of
+// the source object and copying them into the target whenever they are not
+// 'undefined'
+function extend(target, source) {
+    if (source) {
+        for (var key in source) {
+            var val = source[key];
+            if (typeof val !== "undefined") {
+                target[key] = val;
+            }
+        }
+    }
+    return target;
 }
