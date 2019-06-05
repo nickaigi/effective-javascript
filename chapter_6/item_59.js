56e14da3ca0cf0af7483805c448d09d65c61db46
Nickson Kaigi <nickaigi@gmail.com>
2019-06-05 10:39:18 +0300

    done with item 59

--- chapter_6/item_59.js
+++ chapter_6/item_59.js
@@ -45,3 +45,78 @@ BitVector.prototype.enable = function(x) {
         throw new TypeError("expected number or array-like");
     }
 }; // book omits this semi-colon 4th June 2019
+
+/* that last version of enable makes use of 'defensive programming'
+ * which attempts to defend against potential errors with additional check
+ */
+
+// example of 'BitVector' constructor that has a single upfront check
+function BitVector(x) {
+    uint32.or(arraylike).guard(x);
+    // ...
+}
+
+// lets build a utility library of guard objects with the help of a shared
+// prototype object that implements the 'guard' method
+
+var guard = {
+    guard: function(x) {
+        if (!this.test(x)) {
+            throw new TypeError("expected "+ this);
+        }
+    }
+};
+
+// each guard object then implements its own 'test' method and string
+// description for error messages
+
+var uint32 = Object.create(guard);
+
+/*
+ * Zero-fill right shift https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators#%3E%3E%3E_(Zero-fill_right_shift)
+ *
+ * The 'unsigned right shift operator' converts its argument to an unsigned
+ * 32-bit integer before performing a bitwise shift.
+ * shifting by zero bits then has not effect on the integer value
+ */
+uint32.test = function(x) {
+    return typeof x === "number" && x === (x >>> 0);
+};
+
+uint32.toString = function() {
+    return "uint32";
+};
+
+
+// implement the 'arrayLike' guard object
+var arrayLike = Object.create(guard);
+
+arrayLike.test = function(x) {
+    return typeof x === "object" && x && uint32.test(x.length);
+};
+
+arrayLike.toString = function() {
+    return "array-like object";
+};
+
+// we can implement "chaining" methods (item 60) such as 'or' as prototype
+// methods
+guard.or = function(other) {
+    var result = Object.create(guard);
+    var self = this;
+    result.test = function(x) {
+        return self.test(x) || other.test(x);
+    };
+
+    var description = this + " or " + other;
+    result.toString = function() {
+        return description;
+    };
+
+    return result;
+};
+
+/* Take Away: while we have to write more code, that will potentially slow down
+ * code execution, the trade-off is that we can be safe in the thought that we
+ * will spend less time dealing with bugs in future
+ */
