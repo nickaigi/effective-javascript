23a663a9053afcbdcde869555942fc8a697dab24
Nickson Kaigi <nickaigi@gmail.com>
2019-06-07 18:51:43 +0300

    typo fix

--- chapter_7/item_62.js
+++ chapter_7/item_62.js
@@ -1,7 +1,18 @@
 // Use nested or named callbacks for asynchronous sequencing
 
-downloadAsyncI("file.txt", function(file) {
+downloadAsync("file.txt", function(file) {
     console.log("finished");
 });
 
 console.log("starting");
+
+// what if we need to lookup a URL in an asynchronous db and then download the
+// contents of that URL... its impossible to initiate both requests back-to-back
+
+db.lookupAsync("url", function(url) {
+    // ?
+});
+
+downloadAsync(url, function(text) { // error: url is not bound
+    console.log("contents of " + url + ": " + text);
+});
