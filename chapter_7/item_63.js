98167662e9cd6e58aec371527e1754480ed657e0
Nickson Kaigi <nickaigi@gmail.com>
2019-06-10 12:24:04 +0300

    typo fix, nested version

--- chapter_7/item_63.js
+++ chapter_7/item_63.js
@@ -9,8 +9,24 @@ try {
 }
 
 
-downloadAsyn("http://example.com/file.txt", function(text) {
+downloadAsync("http://example.com/file.txt", function(text) {
     console.log("File contents: " + text);
 }, function(error) {
     console.log("Error: " + error);
+});
+
+// nesting the callbacks as explained in item_62
+
+downloadAsync("a.txt", function(a) {
+    downloadAsync("b.txt", function(b) {
+        downloadAsync("c.txt", function(c) {
+            console.log("Contents: " + a + b + c);
+        }, function(error) {
+            console.log("Error" + error);
+        });
+    }, function(error) {  // repeated error-handling logic
+        console.log("Error" + error);
+    });
+}, function(error) {  // repeated error-handling logic
+    console.log("Error" + error);
 });
