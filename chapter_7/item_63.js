3cd683febf00a0080e41a07afe2443c771177dcf
Nickson Kaigi <nickaigi@gmail.com>
2019-06-10 17:17:47 +0300

    DRY

--- chapter_7/item_63.js
+++ chapter_7/item_63.js
@@ -30,3 +30,25 @@ downloadAsync("a.txt", function(a) {
 }, function(error) {  // repeated error-handling logic
     console.log("Error" + error);
 });
+
+// using the same error-handling logic
+// DRY: don't repeat yourself
+function onError(error) {
+    console.log("Error: " + error);
+}
+
+downloadAsync("a.txt", function(a) {
+    downloadAsync("b.txt", function(b) {
+        downloadAsync("c.txt", function(c) {
+            console.log("Contents: " + a + b + c);
+        }, onError);
+    }, onError);
+}, onError);
+
+// we can apply what we learnt in item 62 and item 66
+
+downloadAsync(["a.txt", "b.txt", "c.txt"], function(abc) {
+    console.log("Contents: " + abc[0] + abc[1] + abc[2]);
+}, function(error) {
+    console.log("Error: " + error);
+});
