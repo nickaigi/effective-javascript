98f2f5f8f3f68f39a9d8bff92f2cb8f2bc7a07de
Nickson Kaigi <nickaigi@gmail.com>
2019-06-12 13:16:18 +0300

    initial import

---
+++ chapter_7/item_67.js
@@ -0,0 +1,38 @@
+// Never call Asynchronous callbacks Synchronously
+/* Picture a variation of 'downloadAsync' that keeps a cache(implemented as a
+ * 'Dict' - Item45) to avoid downloading the same file multiple times
+ */
+
+var cache = new Dict();
+
+function downloadCachingAsync(url, onsuccess, onerror) {
+    if (cache.has(url)) {
+        onsuccess(cache.get(url));  // synchronous call
+        return;
+    }
+    return downloadAsync(url, function(file) {
+        cache.set(url, file);
+        onsuccess(file);
+    }, onerror);
+}
+
+downloadAsync("file.txt", function(file) {
+    console.log("finished");
+});
+
+console.log("starting");
+
+downloadCachingAsync("file.txt", function(file) {
+    console.log("finished"); // might happen first
+});
+console.log("starting");
+
+// an application might keep a queue of files remaining to download and
+// display a message to the user
+
+downloadCachingAsync(remaining[0], function(file) {
+    remaining.shift();
+    // ...
+});
+
+status.display("Downloading " + remaining[0] + "...");
