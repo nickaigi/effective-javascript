b4da393c14c3ada6f9054bc97381475f28f44819
Nickson Kaigi <nickaigi@gmail.com>
2019-06-12 12:31:05 +0300

    added an explanation as to why the code has a bug

--- chapter_7/item_66.js
+++ chapter_7/item_66.js
@@ -29,3 +29,33 @@ function downloadAllAsync(urls, onsuccess, onerror) {
         });
     });
 }
+
+// to see what goes wrong, consider this usecase
+
+var filenames = [
+    "huge.txt", // huge file
+    "tiny.txt", // tiny file
+    "medium.txt" // medium-sized file
+];
+
+downloadAllAsync(filenames, function(files) {
+    console.log("Huge file: " + files[0].length);  // tiny
+    console.log("Tiny file: " + files[1].length);  // medium
+    console.log("Medium file: " + files[2].length);  // huge
+}, function(error) {
+    console.log("Error: " + error);
+});
+
+/* since the files are downloaded concurrently, the events can occur in
+ * arbitrary order.
+ *
+ * if tiny.txt completes first, followed by medium.txt and then huge.txt, the
+ * callbacks in 'downloadAllAsync' will be called in a different order than the
+ * they were created in
+ *
+ * But the implementation of 'downloadAllAsync' pushes each intermediate result
+ * onto the end of the result array as soon as it arrives.
+ *
+ * So 'downloadAllAsync' produces an array containing downloaded files stored in
+ * an unknown order
+ */
